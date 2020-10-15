a68394f6b6b3bab9d2953b7dac481a971ad5233a
unknown <vidhya.1701254@srec.ac.in>
2020-10-15 20:46:51 +0530

    Updates

---
+++ precache-manifest.ba4154936ab740529c540b22f8b68b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c195f1f80f729cf781a4b1782322cd",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "139f4939869a5bfb09de",
+    "url": "/sample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d4999c29c72f49e6b210",
+    "url": "/sample/static/js/2.e6b02365.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sample/static/js/2.e6b02365.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139f4939869a5bfb09de",
+    "url": "/sample/static/js/main.53e8b0ea.chunk.js"
+  },
+  {
+    "revision": "008da192540ba44288ac",
+    "url": "/sample/static/js/runtime-main.b578605f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
